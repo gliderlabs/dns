25a408d877170f2d9c7f2337a91cc3453d10e211
Jeff Lindsay <progrium@gmail.com>
2017-08-31 13:14:34 -0500

    cmd.io: pointing alpha to GKE
    Signed-off-by: Jeff Lindsay <progrium@gmail.com>

--- cmd.io.tf
+++ cmd.io.tf
@@ -4,6 +4,19 @@ resource "aws_route53_zone" "cmd_io" {
   comment = "${var._comment}"
 }
 
+// cmd.io
+resource "aws_route53_record" "cmd_io" {
+  zone_id = "${aws_route53_zone.cmd_io.zone_id}"
+  name = "cmd.io"
+  type = "A"
+  alias {
+    name = "d24sdw7s6lpyet.cloudfront.net."
+    zone_id = "Z2FDTNDATAQYW2"
+    evaluate_target_health = false
+  }
+}
+
+// www.cmd.io
 resource "aws_route53_record" "www_cmd_io" {
   zone_id = "${aws_route53_zone.cmd_io.zone_id}"
   name = "www.cmd.io"
@@ -15,29 +28,20 @@ resource "aws_route53_record" "www_cmd_io" {
   }
 }
 
+// alpha.cmd.io
 resource "aws_route53_record" "alpha_cmd_io" {
   zone_id = "${aws_route53_zone.cmd_io.zone_id}"
   name = "alpha.cmd.io"
-  type = "CNAME"
+  type = "A"
   ttl = "300"
-  records = ["ae298a553083611e7a3eb0a59467cbd8-872746419.us-east-1.elb.amazonaws.com."]
+  records = ["35.202.110.23"]
 }
 
+// beta.cmd.io
 resource "aws_route53_record" "beta_cmd_io" {
   zone_id = "${aws_route53_zone.cmd_io.zone_id}"
   name = "beta.cmd.io"
   type = "A"
   ttl = "300"
   records = ["35.194.44.84"]
-}
-
-resource "aws_route53_record" "cmd_io" {
-  zone_id = "${aws_route53_zone.cmd_io.zone_id}"
-  name = "cmd.io"
-  type = "A"
-  alias {
-    name = "d24sdw7s6lpyet.cloudfront.net."
-    zone_id = "Z2FDTNDATAQYW2"
-    evaluate_target_health = false
-  }
 }
